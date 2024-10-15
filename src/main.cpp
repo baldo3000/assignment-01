ed033c40b263d3dd81ddcb7e4967262062874e06
Andrea Baldazzi <andrea.baldazzi7@studio.unibo.it>
2024-10-15 15:32:29 +0200

    Initial state machine structure

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,23 @@
 #include <EnableInterrupt.h>
 #include "functions.h"
 
+enum State
+{
+  INIT,      // game start screen
+  SLEEP,     // board went to sleep after inactivity
+  START,     // game started
+  NEWROUND,  // configuration for next round
+  SELECTION, // player is guessing binary conversion
+  CHECK,     // checking player's inputs
+  GAMEOVER   // end of game
+};
+
 volatile bool led1State;
 volatile bool led2State;
 volatile bool led3State;
 volatile bool led4State;
 int difficulty;
+State state = INIT;
 
 void setup()
 {
@@ -31,7 +43,34 @@ void setup()
 
 void loop()
 {
-  difficulty = difficulty_changer();
-  Serial.println(difficulty);
-  delay(1000);
+  switch (state)
+  {
+  case INIT:
+    // TODO display start message
+    // TODO pulse red led
+    break;
+  case SLEEP:
+    // TODO goin sleep
+    break;
+  case START:
+    // TODO display game start screen
+    // TODO starts round 1
+    break;
+  case NEWROUND:
+    // TODO randomize new turn number
+    // TODO calculate new turn max time
+    break;
+  case SELECTION:
+    // TODO display current turn number to guess
+    // TODO player selects leds with buttons
+    // TODO if time ended check inputs
+    break;
+  case CHECK:
+    // TODO check player's inputs
+    break;
+  case GAMEOVER:
+    // TODO display game over message and score
+    // TODO resets game
+    break;
+  }
 }
